f90c672223f2f24925b71fc1b7587a7192ba11b9
notoriousno <notorious.no@gmail.com>
2015-03-09 19:05:36 -0400

    Update ftpclient.py

--- ftpclient.py
+++ ftpclient.py
@@ -1,70 +1,116 @@
 __author__ = 'nsarker@arinc.com'
-__version__ = '0.01'
+__version__ = '0.02'
 __status__ = 'alpha'
 
 
 from twisted.python.failure import Failure
 from twisted.protocols.ftp import FTPClient as _FTPClient
 from twisted.internet.protocol import ClientCreator, Protocol
-from os import getcwd, path as _path
+from os import getcwd, path as _path, makedirs
 split = _path.split
 exists = _path.exists
 getsize = _path.getsize
 join = _path.join
 
+from twisted.internet.defer import Deferred, DeferredList
 from twisted.internet import reactor
 
 
-#---------- Success ----------#
-def __success(success, ftpConnection):
-    print('[X] Successful Transaction')
-    try:
-        for x in success:
-            print(x)
-    except Exception as e:
-        print(success)
-
-    try:
-        return ftpConnection.quit()
-    except:
-        print('\nX--- Unable to quit the FTP connection upon success.')
-        return ftpConnection
-
-def __successfulGET(success, ftpConnection, ftpFile, localFile):
-    print('[X] Successful GET details:\nFTP File: {0}\nLocal File: {1}\n'.format(ftpFile, localFile))
-    try:
-        return ftpConnection.quit()
-    except:
-        print('\nX--- Unable to quit the FTP connection upon success.')
-        return ftpConnection
-
-
-#---------- Failures ----------#
-def __fail(failure, ftpConnection):
-    print('\nX--- Failed transaction')
-    try:
-        for e in failure:
-            print(e)
-    except Exception as e:
-        print(failure)
-
-    try:
-        return ftpConnection.quit()
-    except:
-        print('\nX--- Unable to quit the FTP connection upon failure.')
-        return ftpConnection
-
-
 #---------- Classes ----------#
-class FTPClient(object):
+class FTPClient(object, _FTPClient):
     """ @not_implemented_yet
     """
+
+    def __init__(self, user='anonymous', pswd='', pasv=True):
+        """"""
+        _FTPClient.__init__(self, username=user, password=pswd, passive=pasv)
+
+    def GET(self, *f, **kwargs):
+        """"""
+        getList = []
+        for files in f:
+            d = Deferred()
+            d.addCallback(self._checkFileTuple, files).addErrback(self.genericFail, 'Failed tuple check')
+            d.addCallback(self._checkLocalPath).addErrback(self.genericFail, 'Wrong local path') # check local path exists
+            d.addCallback(self._getFromFTP).addErrback(self.genericFail, 'Unable to get')
+            getList.append(d)      # add to getList
+            d.callback(None)
+        return DeferredList(getList, consumeErrors=True)
+
+    def _checkFileTuple(self, null, files):
+        """
+        """
+        # files = self.getList.pop()
+        ftpUrl = None
+        localPath = None
+        size = 0
+        fromByte = 0
+        
+        try:
+            ftpUrl, localPath, size, frombyte = files
+        except:
+            try:
+                ftpUrl, localPath, size = files
+            except:
+                try:
+                    ftpUrl, localPath = files
+                except:
+                    raise Exception('X--- Invalid file tuple.')
+        return (ftpUrl, localPath, size, fromByte)
+
+    def _checkLocalPath(self, ftpTuple):
+        """ Checks whether or not the local path exists. If not, this fn will attempt to create it.
+        """
+        ftpUrl, localPath, size, fromByte = ftpTuple
+        pth, fl = split(localPath)
+        if not exists(pth):
+            try:
+                makedirs(pth)       # create dir if ! exist
+                return ftpTuple
+            except OSError as e:
+                return Failure(e)   # do errback if unable to create dir
+            except Exception as e:
+                return Failure(e)   # do errback if unable to create dir
+        else:
+            if exists(localPath):
+                fromByte = getsize(localPath)       # file exists so get the remaining bytes from FTP
+            return (ftpUrl, localPath, size, fromByte)
+
+    def _getFromFTP(self, ftpTuple):
+        """
+        """
+        ftpUrl, localPath, size, fromByte = ftpTuple
+        d = self.retrieveFile(path = ftpUrl,
+            protocol = FileBuffer(
+                    localPath, size),
+            offset = fromByte)
+        d.addCallbacks(self.successfulGET, callbackArgs=[ftpTuple],
+            errback=self.failedGET, errbackArgs=[ftpTuple])
+        return d
+
+
+    #---------- Success Callbacks ----------#
+    def successfulGET(self, result, ftpTuple):
+        """"""
+        self.quit()
+        return ftpTuple
+
+
+    #---------- Failure Callbacks ----------#
+    def genericFail(self, result, msg):
+        self.quit()
+        return Failure(Exception('X--- {0}'.format(msg)))
+
+    def failedGET(self, result, ftpTuple):
+        """ """
+        self.quit()
+        return Failure(ftpTuple)
 
 
 class FileBuffer(Protocol):
     """
     """
-    def __init__(self, lclPath=''):
+    def __init__(self, lclPath='', size=0):
         """
         :param name: Name of the file. Used to store on local drive.
         :param path: Local path that will store the file.
@@ -73,13 +119,13 @@ class FileBuffer(Protocol):
         if self.path == '':
             self.path = join(getcwd())      # set default path to the current directory
         self.localPath = join(lclPath)
-        try:
-            assert(_path.isdir(self.path))      # check if dir exists
-            if not exists(self.localPath):
-                with open(self.localPath, 'wb') as _file:
-                    _file.write(b'')        # create file if doesn't exist
-        except Exception as e:
-            raise Exception('X--- File path doesnt exist')      # dir or file doesn't exist
+        # try:
+            # assert(_path.isdir(self.path))      # check if dir exists
+            # if not exists(self.localPath):
+                # with open(self.localPath, 'wb') as _file:
+                    # _file.write(b'')        # create file if doesn't exist
+        # except Exception as e:
+            # raise Exception('X--- File path doesnt exist')      # dir or file doesn't exist
 
     def connectionMade(self):
         self.transport.registerProducer(self.transport, streaming=True)     # Registers the Producer (this case is the FTP data conn)
@@ -96,58 +142,6 @@ class FileBuffer(Protocol):
         finally:
             self.transport.resumeProducing()
 
-
-#---------- Get Function ----------#
-def __getFile(ftpConnection, url, lclPath=None):
-    """ This function will actually retrieve the file and transfer it to a local destination.
-
-    @fix This fn is inefficient. Must find better way of not downloading something when file is complete
-
-    :param ftpConnection:
-    :param url:
-    :param lclPath:
-    :return: FTPConnection
-    """
-    offsetByte = 0
-
-    if not lclPath:
-        pth, fileName = split(url)
-        lclPath = join(getcwd(), fileName)      # get cwd path
-
-    if not exists(lclPath):
-        # with open(filePath, 'wb') as _file:
-        #     _file.write(b'')        # create file if doesn't exist
-        offsetByte = 0      # start download from the beginning
-    else:
-        offsetByte = getsize(lclPath)
-
-    localFile = None
-    try:
-        localFile = FileBuffer(lclPath=lclPath)
-    except Exception as e:
-        Failure(e)
-
-    ftpConnection.retrieveFile(
-        url,
-        localFile,
-        offset = offsetByte).addCallbacks(__successfulGET, callbackArgs=[ftpConnection, localFile, url],
-                                          errback=__fail, errbackArgs=[ftpConnection])
-    return ftpConnection
-
-def get(url, localFile=None, size=None, offset=0, user='anonymous', pswd='', passive=True, host='127.0.0.1', port=21):
-    """
-
-    :param fileNPath:
-    :param size:
-    :param USER:
-    :param PSWD:
-    :param passive:
-    :return: FTPConnection
-    """
-    ftpClient = ClientCreator(reactor, _FTPClient, user, pswd, passive=passive)
-    ftpConnection = ftpClient.connectTCP(host, port)
-    ftpConnection.addCallback(__getFile, url=url, lclPath=localFile).addErrback(__fail, ftpClient)  # @fix Use better Errback
-    return ftpConnection
 
 
 #---------- Store Function ----------#
@@ -208,16 +202,38 @@ def put(localPath, ftpPath, user='anonymous', pswd='', passive=True, bitLimit=10
 if __name__=='__main__':
     HOST = 'localhost'
     PORT = 21
-    USER = 'notorious'
+    USER = 'noman'
     PSWD = 'qwerty'
+    pasv = True
 
-    #----- Basic get from FTP -----#
-    # get('large.zip', localFile='LARGE.zip', user=USER, pswd=PSWD, host=HOST, port=PORT)
+    #----- New Class based -----#
+    def getFiles(client, *files, **kwargs):
+        """
+        :param files: Tuples (ftp_url, local_path, size_of_file, byte_offset) of which size of file and offset are **optional**.
+        :param callback: A callback chain that will be executed after files are retrieved. (type: Deferred)
+        """
+        after = kwargs.get('after', Deferred())
+        d = client.GET(*files, **kwargs).chainDeferred(after)
+        print('')
 
-    #----- Get from file from a subfolder and put in a local subfolder ----#
-    # get('test/large.zip', 'dir/LARGE.zip', user=USER, pswd=PSWD)
+    def afterGET(results):
+        """"""
+        for outcome in results:
+            res, out = outcome
+            if not res:
+                print out.value
+            else:
+                print out
 
+    ftpcli = ClientCreator(reactor, FTPClient, USER, PSWD, pasv)
+    ftpconn = ftpcli.connectTCP(HOST, PORT)
+    d = Deferred()
+    d.addCallback(afterGET)
+    ftpconn.addCallback(getFiles,
+        ('large.zip', 'blh/LARGE.zip'),
+        # ('scream.png', 'blah/scream.png'),
+        # ('1', '2', 4, 5, 5, 6),
+        ('ubntu.iso', 'blah/ubuntu.iso'),
+        after = d)
 
-    #----- Basic put to FTP -----#
-    put('large.zip', ftpPath='test/large.zip', user=USER, pswd=PSWD, host=HOST, port=PORT)
     reactor.run()
